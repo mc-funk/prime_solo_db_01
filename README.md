0671a05e69df1b4a174566a04295dcb29d5e5466
Michelle C. Funk <michelle@michellefunk.com>
2015-05-18 08:35:09 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Prime Example Passport assignment - Weekend challenge 
+
+Very behind on this due to impending Jun 1 move. Will do my best to catch up ASAP. 
+
+Stuck on authentication error. 
--- models/user.js
+++ models/user.js
@@ -10,9 +10,12 @@ var UserSchema = new Schema ({
 
 UserSchema.pre('save', function(next) {
     var user = this;
+
     if (!user.isModified('password')) return next();
+
     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
         if (err) return next (err);
+
         bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) return next(err);
             user.password = hash;
