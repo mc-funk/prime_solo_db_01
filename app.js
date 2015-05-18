61700d835ec8e60cbba43a57fc7d48da60265201
Michelle C. Funk <michelle@michellefunk.com>
2015-05-18 08:19:14 -0500

    Authentication failing at register

--- app.js
+++ app.js
@@ -4,15 +4,70 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var session = require('express-session');
+var User = require('./models/user');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var register = require('./routes/register');
 
 var app = express();
+var mongoURI = "mongodb://localhost:27017/prime_example_passport";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function(err) {
+  console.log('mongodb connection error', err)
+});
+
+MongoDB.on('open', function(err) {
+  console.log('mongodb connection open')
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user){
+    if(err) done(err);
+    done(null, user);
+  });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({
+  secret: 'secret',
+  key: 'user',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000, secure: false}
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use('local', new localStrategy(
+    { passReqToCallback : true, _usernameField: 'username'},
+    function(req, username, password, done){
+      User.findOne({ username: username }, function(err, user) {
+        if (err) throw err;
+        if (!user)
+          return done(null, false, {message: 'Incorrect username and password.'});
+
+        user.comparePassword(password, function(err, isMatch) {
+          if (err) throw err;
+          if(isMatch) return done(null, user);
+          else
+            done(null, false, { message: 'Incorrect username and password.' });
+    });
+  });
+}));
+app.use('/register', register);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req,res,next){
+  res.sendFile(path.resolve(__dirname, '../views/index.html'));
 });
+
+router.post('/',
+    passport.authenticate('local', {
+      successRedirect: '/users',
+      failureRedirect: '/'
+    })
+);
 
 module.exports = router;
